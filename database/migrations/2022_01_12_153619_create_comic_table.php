f9bbe4456a9d0467e741e70e5e4a6b66f63c4cd3
Eddishot <Eddi_sl94@hotmail.it>
2022-01-12 17:26:40 +0100

    add migration and model

---
+++ database/migrations/2022_01_12_153619_create_comic_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comic', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 255); 
+            $table->text("description");
+            $table->float("price",8, 2 );
+            $table->string("series", 255);
+            $table->year("sale_date",);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comic');
+    }
+}
